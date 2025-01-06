35c71cfa6c49ef9374432f8b0437c1cbe6b639c3
Robin Brown <me@esoterra.dev>
2025-01-05 23:14:04 -0500

    feat: cleanup, make lib configurable, fix list deletion

---
+++ editor/README.md
@@ -0,0 +1,25 @@
+# Editor
+
+## TODO
+
+[ ] Nested lists
+[ ] Creating sections
+[ ] Moving nodes
+
+## References
+
+https://developer.mozilla.org/en-US/docs/Web/API/Element
+https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector
+https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelectorAll
+https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/contenteditable
+https://developer.mozilla.org/en-US/docs/Web/API/Element/beforeinput_event
+
+
+```js
+function markDirty(element) {
+    element.dataset.dirty = true;
+}
+function isDirty(element) {
+    return element.dataset.dirty || false;
+}
+```
--- editor/editor.js
+++ editor/editor.js
@@ -1,7 +1,3 @@
-// https://developer.mozilla.org/en-US/docs/Web/API/Element
-// https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector
-// https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelectorAll
-
 const ARTICLE_TAG = "ARTICLE";
 const SECTION_TAG = "SECTION";
 const P_TAG = "P";
@@ -10,13 +6,13 @@ const OL_TAG = "OL";
 const LI_TAG = "LI";
 
 function isElement(node) {
-    node.nodeType === Node.ELEMENT_NODE
+    node.nodeType === Node.ELEMENT_NODE;
 }
 function isContainer(element) {
     return [ARTICLE_TAG, SECTION_TAG].includes(element.tagName);
 }
-function isContainerContent(element) {
-    return [SECTION_TAG, P_TAG, UL_TAG, OL_TAG].includes(element.tagName)
+function isContainerItem(element) {
+    return [SECTION_TAG, P_TAG, UL_TAG, OL_TAG].includes(element.tagName);
 }
 function isArticle(element) {
     return element.tagName === ARTICLE_TAG;
@@ -25,7 +21,7 @@ function isSection(element) {
     return element.tagName === SECTION_TAG;
 }
 function isHeading(element) {
-    return ["H1", "H2", "H3", "H4", "H5", "H6"].includes(element.tagName)
+    return ["H1", "H2", "H3", "H4", "H5", "H6"].includes(element.tagName);
 }
 function headingTagForSection(section) {
     let depth = 0;
@@ -39,68 +35,25 @@ function isParagraph(element) {
     return element.tagName == P_TAG;
 }
 function isList(element) {
-    return [UL_TAG, OL_TAG].includes(element.tagName)
+    return [UL_TAG, OL_TAG].includes(element.tagName);
 }
 function isListItem(element) {
     return element.tagName === LI_TAG;
 }
 
-function validateDocument(editorRoot) {
-    const invalidElements = [];
-    for (const child of editorRoot.children) {
-        if (!isContainerContent(child)) {
-            invalidElements.push(child)
-        }
+class Context {
+    constructor(editorRoot, idPrefix) {
+        this.editorRoot = editorRoot;
+        this.idPrefix = idPrefix;
+        this.nextId = 0;
     }
-    if (invalidElements.length !== 0) {
-        console.log(invalidElements)
-        throw new Error("Editor document contains invalid element")
+    newID() {
+        const id = `${this.idPrefix}${this.nextId}`;
+        this.nextId += 1;
+        return id;
     }
-    if (editorRoot.children.length === 0) {
-        throw new Error("Editor document does not contain any elements")
-    }
-    editorRoot.querySelectorAll("section").forEach((section) => {
-        if (section.firstElementChild === null) {
-            console.log(section);
-            throw new Error("Section does not contain header")
-        }
-        const hTag = headingTagForSection(section);
-        let first = true;
-        for (const child of section.children) {
-            if (first) {
-                first = false;
-                if (child.tagName !== hTag) {
-                    console.log(child)
-                    throw new Error("Section does not start with correct header")
-                }
-            } else {
-                if (!isContainerContent(child)) {
-                    console.log(child)
-                    throw new Error("Section contains invalid element")
-                }
-            }
-        }
-    })
-    editorRoot.querySelectorAll("ul,ol").forEach((listElement) => {
-        if (listElement.children.length === 0) {
-            console.log(listElement);
-            throw new Error("List has no children")
-        }
-        for (const child of listElement.children) {
-            if (child.tagName !== LI_TAG) {
-                console.log(e)
-                throw new Error("List contains invalid element")
-            }
-        }
-    })
-}
-
-// https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/contenteditable
-function updateDocumentAttributes(editorRoot) {
-    var nextId = 0;
-    editorRoot.querySelectorAll("section,p,ul,ol,li").forEach((element) => {
-        element.id = `wedit-${nextId}`;
-        nextId += 1;
+    initElement(element) {
+        element.id = this.newID();
         if (isSection(element)) {
             const heading = element.firstElementChild;
             heading.contentEditable = true;
@@ -110,74 +63,141 @@ function updateDocumentAttributes(editorRoot) {
             element.contentEditable = true;
             element.spellcheck = true;
         }
+    }
+    newElement(tagName) {
+        const newElement = document.createElement(tagName);
+        if (isParagraph(newElement) || isListItem(newElement) || isHeading(newElement)) {
+            newElement.spellcheck = true;
+            newElement.contentEditable = true;
+        }
+        newElement.id = this.newID();
+        return newElement;
+    }
+}
+
+function validateDocument(ctx) {
+    const editorRoot = ctx.editorRoot;
+    const invalidElements = [];
+    for (const child of editorRoot.children) {
+        if (!isContainerItem(child)) {
+            invalidElements.push(child);
+        }
+    }
+    if (invalidElements.length !== 0) {
+        console.log(invalidElements);
+        throw new Error("Editor document contains invalid element");
+    }
+    if (editorRoot.children.length === 0) {
+        throw new Error("Editor document does not contain any elements");
+    }
+    editorRoot.querySelectorAll("section").forEach((section) => {
+        if (section.firstElementChild === null) {
+            console.log(section);
+            throw new Error("Section does not contain header");
+        }
+        const hTag = headingTagForSection(section);
+        let first = true;
+        for (const child of section.children) {
+            if (first) {
+                first = false;
+                if (child.tagName !== hTag) {
+                    console.log(child);
+                    throw new Error("Section does not start with correct header");
+                }
+            } else {
+                if (!isContainerItem(child)) {
+                    console.log(child);
+                    throw new Error("Section contains invalid element");
+                }
+            }
+        }
+    })
+    editorRoot.querySelectorAll("ul,ol").forEach((listElement) => {
+        if (listElement.children.length === 0) {
+            console.log(listElement);
+            throw new Error("List has no children");
+        }
+        for (const child of listElement.children) {
+            if (child.tagName !== LI_TAG) {
+                console.log(child);
+                throw new Error("List contains invalid element");
+            }
+        }
     })
 }
 
-// https://developer.mozilla.org/en-US/docs/Web/API/Element/beforeinput_event
-function beforeInputListener(event) {
-    console.log("beforeinput:")
-    console.log(event)
+function updateDocumentAttributes(ctx) {
+    ctx.editorRoot.querySelectorAll("section,p,ul,ol,li").forEach((element) => ctx.initElement(element))
+}
+
+function beforeInputListener(ctx, event) {
+    console.log("beforeinput:");
+    console.log(event);
 
     const target = event.target;
-    const targetTag = event.target.tagName;
-    const targetContent = event.target.textContent;
+    const targetTag = target.tagName;
+    const targetContent = target.textContent;
     const data = event.data;
+
     if (event.inputType === "insertParagraph") {
         if (isListItem(target)) {
             if (targetContent === "") {
-                splitList(target)
+                splitList(ctx, target);
                 event.preventDefault();
                 return;
             }
         }
-        const newTag = targetTag.startsWith("H") ? "P" : targetTag;
-        const newElement = document.createElement(newTag)
-        newElement.spellcheck = true
-        newElement.contentEditable = true
-        event.target.after(newElement)
-        document.getSelection().setPosition(newElement)
-        event.preventDefault()
+        const newTag = isHeading(target) ? P_TAG : targetTag;
+        const newElement = ctx.newElement(newTag);
+        console.log({ newTag, newElement });
+        target.after(newElement);
+        document.getSelection().setPosition(newElement);
+        event.preventDefault();
         return;
     }
     if (event.inputType === "deleteContentBackward") {
-        if (event.target.textContent === "") {
+        if (targetContent === "") {
             const prev = prevEditable(event.target);
-            selectEnd(prev)
-            event.target.remove()
-            event.preventDefault()
+            selectEnd(prev);
+            if (isListItem(target) && target.parentElement.childElementCount === 1) {
+                target.parentElement.remove();
+            } else {
+                target.remove();
+            }
+            event.preventDefault();
             return;
         }
     }
     if (event.inputType === "insertText") {
         if (targetTag === P_TAG && targetContent === "*" && data === " ") {
-            transmuteIntoList(event, UL_TAG);
-            event.preventDefault()
+            transmuteIntoList(ctx, event, UL_TAG);
+            event.preventDefault();
             return;
         }
         if (targetTag === P_TAG && targetContent === "1." && data === " ") {
-            transmuteIntoList(event, OL_TAG);
-            event.preventDefault()
+            transmuteIntoList(ctx, event, OL_TAG);
+            event.preventDefault();
             return;
+        }
+        if (targetTag === P_TAG && targetContent === "#" && data === " ") {
+            console.log("TODO: create section heading")
         }
     }
 }
 
-function transmuteIntoList(event, tagName) {
-    const prev = event.target.previousElementSibling;
+function transmuteIntoList(ctx, event, tagName) {
     // Create and insert new list
-    const newList = document.createElement(tagName);
-    prev.after(newList);
+    const newList = ctx.newElement(tagName);
+    event.target.replaceWith(newList);
     // Create and insert new list item
-    const newListItem = document.createElement(LI_TAG);
-    newListItem.spellcheck = true
-    newListItem.contentEditable = true
-    newList.appendChild(newListItem)
+    const newListItem = ctx.newElement(LI_TAG);
+    newList.appendChild(newListItem);
     // Focus new list item and remove old tag
-    document.getSelection().setPosition(newListItem)
-    event.target.remove()
+    document.getSelection().setPosition(newListItem);
+    event.target.remove();
 }
 
-function splitList(element) {
+function splitList(ctx, element) {
     const parentList = element.parentElement;
     // Add all elements after this one in the list to the `after` list and remove them from the parent
     let current = element.nextElementSibling;
@@ -187,23 +207,21 @@ function splitList(element) {
         current = current.nextElementSibling;
     }
     // Create and select the new paragraph element
-    element.remove()
-    const newParagraph = document.createElement(P_TAG);
-    newParagraph.spellcheck = true
-    newParagraph.contentEditable = true
+    element.remove();
+    const newParagraph = ctx.newElement(P_TAG);
     parentList.after(newParagraph);
-    document.getSelection().setPosition(newParagraph)
+    document.getSelection().setPosition(newParagraph);
     // Create the list for the remainder
-    const newList = document.createElement(parentList.tagName);
+    const newList = ctx.newElement(parentList.tagName);
     for (const current of remainder) {
-        newList.appendChild(current)
+        newList.appendChild(current);
     }
-    newParagraph.after(newList)
+    newParagraph.after(newList);
 }
 
 function keydownListener(event) {
-    console.log("keydown:")
-    console.log(event)
+    console.log("keydown:");
+    console.log(event);
 
     const target = event.target;
     const selection = document.getSelection();
@@ -212,28 +230,28 @@ function keydownListener(event) {
         if (prev === null) {
             return;
         }
-        selectEnd(prev)
-        scrollToIfNeeded(prev)
-        event.preventDefault()
+        selectEnd(prev);
+        scrollToIfNeeded(prev);
+        event.preventDefault();
         return;
     }
     if (event.key === "ArrowDown" && atEnd(event)) {
-        const next = nextEditable(event.target);
+        const next = nextEditable(target);
         if (next === null) {
             return;
         }
-        selection.setPosition(next)
-        scrollToIfNeeded(next)
-        event.preventDefault()
+        selection.setPosition(next);
+        scrollToIfNeeded(next);
+        event.preventDefault();
         return;
     }
     if (event.key === "Tab") {
         if (event.shiftKey) {
-            console.log("TODO: Indent List")
+            console.log("TODO: Indent List");
         } else {
-            console.log("TODO: Dedent List")
+            console.log("TODO: Dedent List");
         }
-        event.preventDefault()
+        event.preventDefault();
     }
 }
 
@@ -246,7 +264,7 @@ function atStart(event) {
         const selectionRange = selection.getRangeAt(0);
         return selectionRange.startOffset === 0;
     }
-    console.warn("More than one selection!!")
+    console.warn("More than one selection!!");
     return false;
 }
 
@@ -267,20 +285,20 @@ function atEnd(event) {
 function selectEnd(element) {
     const selection = document.getSelection();
     if (element.textContent === "") {
-        selection.setPosition(element)
+        selection.setPosition(element);
         return;
     }
     if (element.lastChild !== null) {
-        selection.setPosition(element.lastChild, element.lastChild.length)
+        selection.setPosition(element.lastChild, element.lastChild.length);
         return;
     }
-    selection.setPosition(element)
+    selection.setPosition(element);
 }
 
 function scrollToIfNeeded(element) {
     const rect = element.getBoundingClientRect();
     if (rect.y < 0 || rect.bottom > document.documentElement.clientHeight) {
-        element.scrollIntoView()
+        element.scrollIntoView();
     }
 }
 
@@ -346,14 +364,13 @@ function nextFromSectionChild(element) {
     return null;
 }
 
-function onload() {
-    console.log("init start")
-    const editorRoot = document.getElementById("wedit-root");
-    validateDocument(editorRoot)
-    updateDocumentAttributes(editorRoot)
-    editorRoot.addEventListener("beforeinput", beforeInputListener)
-    editorRoot.addEventListener("keydown", keydownListener)
-    console.log("init finished")
+function init(editorRoot, idPrefix) {
+    console.log("init start");
+    const ctx = new Context(editorRoot, idPrefix)
+    console.log(ctx)
+    validateDocument(ctx);
+    updateDocumentAttributes(ctx);
+    editorRoot.addEventListener("beforeinput", (event) => beforeInputListener(ctx, event));
+    editorRoot.addEventListener("keydown", keydownListener);
+    console.log("init finished");
 }
-
-document.addEventListener('DOMContentLoaded', onload, true);
